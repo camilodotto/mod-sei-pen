194f21223506808c9df4b9080c965ce581ed98d2
Camilo Reis Dotto <camilo.dotto@dataprev.gov.br>
2025-12-19 14:24:20 -0300

    GuzzleHttp\Client verify false

--- src/rn/ProcessoEletronicoRN.php
+++ src/rn/ProcessoEletronicoRN.php
@@ -1164,6 +1164,7 @@ class ProcessoEletronicoRN extends InfraRN
             'headers'  => $arrheaders,
             'timeout'  => self::WS_TIMEOUT_CONEXAO,
             'cert'     => [$strLocalizacaoCertificadoDigital, $strSenhaCertificadoDigital],
+            'verify'   => false,
         ]);
 
 
