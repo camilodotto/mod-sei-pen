0457e3897fb306e9d134d3b3049f20e615087f64
Camilo Reis Dotto <camilo.dotto@dataprev.gov.br>
2025-12-23 18:18:48 -0300

    add log.patch

--- src/rn/ProcessoEletronicoRN.php
+++ src/rn/ProcessoEletronicoRN.php
@@ -2,11 +2,14 @@
 
 $dirSeiVendor = !defined("DIR_SEI_VENDOR") ? getenv("DIR_SEI_VENDOR") ?:  __DIR__ . "/../vendor" : DIR_SEI_VENDOR;
 require_once $dirSeiVendor . '/autoload.php';
-
+use Psr\Http\Message\RequestInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Utils;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Handler\CurlHandler;
 
 class ProcessoNaoPodeSerDesbloqueadoException extends Exception {}
 
@@ -130,13 +133,34 @@ class ProcessoEletronicoRN extends InfraRN
       'Accept' => '*/*',
       'Content-Type' => 'application/json',
     ];
+    $handler = new CurlHandler();
+    $stack = HandlerStack::create($handler);
     
+    $middleware = Middleware::tap(function (RequestInterface $request) {
+        $timestamp=time();
+        $body = (string) $request->getBody();
+        if (!empty($body)) {
+            $body = json_decode($body, JSON_OBJECT_AS_ARRAY);
+        }
+        $arrayRequest = [
+          'url' => $request->getUri(),
+          'body' => $body,
+          'headers' => $request->getHeaders(),
+        ];
+        @file_put_contents("/var/log/sei/Tramita.log", date("d/m/Y H:i:s") . "- Set:\nTime: {$timestamp}\nBody:\n". var_export($arrayRequest, true) ."\n----------\n", FILE_APPEND);
+
+    });
+    $stack->push($middleware);
+    $debug = fopen("/var/log/sei/debug_requests.txt", "a+");
+
     $this->strClientGuzzle = new Client([
       'base_uri' => $this->strBaseUri,
       'timeout'  => self::WS_TIMEOUT_CONEXAO,
       'headers'  => $this->arrheaders,
       'cert'     => [$strLocalizacaoCertificadoDigital, $strSenhaCertificadoDigital],
       'verify'   => false,
+      'handler' => $stack,
+      'debug' => $debug,
     ]);
   }
 
