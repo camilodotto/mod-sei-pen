6008a36775c381a8ae6947ac18ef8d25d07c8650
Camilo Reis Dotto <camilo.dotto@dataprev.gov.br>
2025-12-30 09:59:40 -0300

    teste

--- src/rn/ProcedimentoAndamentoRN.php
+++ src/rn/ProcedimentoAndamentoRN.php
@@ -110,7 +110,7 @@ class ProcedimentoAndamentoRN extends InfraRN
      * @param array $parArrChavesSincronizacao Chaves que serã utilizadas na sincronização do processamento
      * @return void
      */
-  public function sinalizarInicioRecebimentoControlado($parArrChavesSincronizacao)
+  public function sinalizarInicioRecebimento($parArrChavesSincronizacao)
     {
      
       $strNumeroRegistro = $parArrChavesSincronizacao["NumeroRegistro"];
